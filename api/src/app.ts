9bc00d1ba79669f3d91c0b799443a5f8677c100d
Fredrik Johansen <fredrik1206@gmail.com>
2024-06-20 22:16:53 +0200

    feat(api): adds api main handler

---
+++ api/src/app.ts
@@ -0,0 +1,64 @@
+import Fastify, { FastifyInstance } from "fastify";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+import fastifyCors from "@fastify/cors";
+import { swaggerConfig, swaggerUiConfig } from "./plugins/swagger";
+import { config } from "./config";
+import { logger } from "./logger";
+import { BaseError } from "./errors";
+import { routes } from "./routes";
+import { socketPlugin } from "./plugins/socket";
+import { sockets } from "./sockets";
+
+const app: FastifyInstance = Fastify({
+  logger: true,
+});
+
+app
+  .register(fastifySwagger, swaggerConfig)
+  .register(fastifySwaggerUi, swaggerUiConfig)
+  .register(fastifyCors, {
+    origin: config.website.baseUrl,
+    methods: ["GET", "POST"],
+    maxAge: 86400,
+    credentials: true,
+  })
+  .register(socketPlugin, {})
+  .after(() => {
+    app.register(sockets);
+    app.register(routes);
+  });
+
+app.setErrorHandler((error, _, res) => {
+  // Catch all BaseErrors and send back their payload
+  if (error instanceof BaseError) {
+    return res.status(error.statusCode).send(error.toJSON());
+  }
+
+  // If any other error occurs, catch it and return a fixed error message
+  logger.fatal({ error }, "Unknown error occurred");
+  return res.status(500).send({
+    code: "unknown-error",
+    message: "An unknown error occurred",
+  });
+});
+
+app.listen(
+  { port: config.api.port, host: config.api.host },
+  (err: Error | null) => {
+    if (err) {
+      app.log.error(err);
+      process.exit(1);
+    }
+  },
+);
+
+process.on("SIGINT", () => {
+  app.log.warn(`SIGINT signal detected, terminating service`);
+  app.close();
+});
+
+process.on("SIGTERM", () => {
+  app.log.warn(`SIGTERM signal detected, terminating service`);
+  app.close();
+});
