79cceabdba4b9d8d9bb863d34ee460d8380874bf
Fredrik Johansen <fredrik1206@gmail.com>
2024-06-21 09:19:43 +0200

    feat(app): adds shadcn-ui button component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@shadcn-ui/components/ui/button";
+
 export const App = () => {
   return (
     <>
       <h1 className="text-lg">Hello World</h1>
+      <Button variant="default">Button</Button>
     </>
   );
 };
