1dfc5cb60cdeb9bd1eb68b78d18e9c1f374de383
Fredrik Johansen <fredrik1206@gmail.com>
2024-06-21 09:37:03 +0200

    feat(app): adds theme provider and selector to app

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,15 @@
 import { Button } from "@shadcn-ui/components/ui/button";
+import { ThemeProvider } from "./providers/Theme";
+import { ThemeSelector } from "./components/ThemeSelector";
 
 export const App = () => {
   return (
     <>
-      <h1 className="text-lg">Hello World</h1>
-      <Button variant="default">Button</Button>
+      <ThemeProvider>
+        <h1 className="text-lg">Hello World</h1>
+        <Button variant="default">Button</Button>
+        <ThemeSelector />
+      </ThemeProvider>
     </>
   );
 };
